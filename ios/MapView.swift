5316e99e9663b76c3ec4d75292d95cc59e671988
Onder Akbas <onderakbas@Onders-Mac-mini.local>
2019-10-05 17:23:47 +0300

    Initial commit

---
+++ ios/MapView.swift
@@ -0,0 +1,9 @@
+//
+//  MapView.swift
+//  reactNativeSwiftMapView
+//
+//  Created by Onder Akbas on 5.10.2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ ios/NativeBridge.m
@@ -0,0 +1,9 @@
+//
+//  NativeBridge.m
+//  reactNativeSwiftMapView
+//
+//  Created by Onder Akbas on 5.10.2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
